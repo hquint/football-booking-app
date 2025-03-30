bd3e146c62270b15a840c17fcd85b6883812fa15
Helio <hquintanilhajr@gmail.com>
2025-03-30 15:22:57 +0200

    Add backend logic to get thursdays and attendance

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,15 +4,19 @@ from .database import Base
 
 class Player(Base):
     __tablename__ = "players"
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     position = Column(String, nullable=True)
 
+    attendances = relationship("Attendance", back_populates="player")
+
 class Attendance(Base):
     __tablename__ = "attendance"
+
     id = Column(Integer, primary_key=True, index=True)
     player_id = Column(Integer, ForeignKey("players.id"))
     date = Column(Date, nullable=False)
-    is_coming = Column(Boolean, default=False)
-    
-    player = relationship("Player")
+    status = Column(String, nullable=False)  # "yes", "no", "maybe"
+
+    player = relationship("Player", back_populates="attendances")
